fd16e6ca2e265ac1ec3c24d61f9c3f121459dc4c
chrisIyer <hellohotbox420@gmail.com>
2023-03-16 17:51:05 -0400

    done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { useState, useEffect } from 'react'
 import TodoList from './components/TodoList/TodoList'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import {
+  DndContext,
+  closestCenter
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
+import { SortableItem } from './components/SortableItem';
 
 
 export default function App(){
+    const [languages, setLanguages ] = useState(["JavaScript", "Python", "TypeScript"]);
     const [todos, setTodos] = useState([])
     const [completedTodos, setCompletedTodos] = useState([])
     const [newTodo, setNewTodo] = useState({
@@ -91,15 +105,43 @@ export default function App(){
     }, [])
     return(
         <>
-            <TodoList
-            newTodo={newTodo}
-            setNewTodo={setNewTodo}
-            createTodo={createTodo}
-            todos={todos}
-            moveToCompleted={moveToCompleted}
-            completedTodos={completedTodos}
-            deleteTodo={deleteTodo}
-            />
+        <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <Container className="p-3" style={{"width": "50%"}} align="center">
+        <h3>The best programming languages!</h3>
+        <SortableContext
+          items={todos}
+          strategy={verticalListSortingStrategy}
+        >
+          {/* We need components that use the useSortable hook */}
+          {todos.map(todo => <SortableItem key={todo._id} id={todo.title}/>)}
+        </SortableContext>
+      </Container>
+    </DndContext>
+
         </>
     )
+    
+    function handleDragEnd(event) {
+        console.log("Drag end called");
+        const {active, over} = event;
+        console.log("ACTIVE: " + active.id);
+        console.log("OVER :" + over.id);
+    
+        if(active.id !== over.id) {
+          setLanguages((items) => {
+            const activeIndex = items.indexOf(active.id);
+            const overIndex = items.indexOf(over.id);
+            console.log(arrayMove(items, activeIndex, overIndex));
+            return arrayMove(items, activeIndex, overIndex);
+            // items: [2, 3, 1]   0  -> 2
+            // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1] 
+          });
+          
+        }
+      }
+
+    
 }
